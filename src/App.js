a96f72946d7f4fe25be16aa269624c3a4ad03be0
Joe Zucco <josephzucco@gmail.com>
2021-12-09 13:53:22 -0500

    establish basic input with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import "./App.css";
 
-import './App.css';
+//components
+import InputTodo from "./components/InputTodo";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='container'>
+        <InputTodo />
+      </div>
     </div>
   );
 }
--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,9 +1,13 @@
 const InputTodo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <h1 className='text-center mt-5'>Input Todo</h1>
+      <form className='d-flex mt-5'>
+        <input type='text' className='form-control' />
+        <button className='btn btn-success'>Add</button>
+      </form>
+    </>
+  );
+};
 
-export default InputTodo
+export default InputTodo;
