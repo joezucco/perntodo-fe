b045bfcb496177c8ef3c0d39ddbb4822ee1a253c
Joe Zucco <josephzucco@gmail.com>
2021-12-13 12:25:17 -0500

    import basic bootstrap table

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -1,8 +1,34 @@
 const ListTodos = () => {
-    return (
-<h1>List Todos</h1>
-    )
-}
+  return (
+    <>
+      <table class='table'>
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr>
+          <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+            <td>mary@example.com</td>
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-export default ListTodos
-
+export default ListTodos;
