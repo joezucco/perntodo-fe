9fddbf16d0a6dbf625497001891d609b4e43a95d
Joe Zucco <josephzucco@gmail.com>
2021-12-13 12:52:29 -0500

    deleteTodo successful

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -3,6 +3,18 @@ import { useEffect, useState } from "react";
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  //delete todo function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:3000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:3000/todos");
@@ -17,8 +29,6 @@ const ListTodos = () => {
   useEffect(() => {
     getTodos();
   }, []);
-
-  console.log(todos);
   return (
     <>
       <table class='table mt-5 text-center'>
@@ -36,10 +46,17 @@ const ListTodos = () => {
             <td>john@example.com</td>
           </tr> */}
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
